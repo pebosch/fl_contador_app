4ed959cc30a20e97c90e2585e17a381479bc874b
Pedro Bosch <pedro@andared.com>
2025-10-21 09:48:54 +0200

    floatingActionButton

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,9 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    const tamano30 = TextStyle(fontSize: 30);
+
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
       appBar: AppBar(
@@ -15,11 +18,19 @@ class HomeScreen extends StatelessWidget {
           child: const Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text('Deliver features faster'),
+              Text('Deliver features faster', style: tamano30),
               SizedBox(height: 100),
-              Text('Craft beautiful UIs'),
+              Text('Craft beautiful UIs',  style: tamano30),
             ],
           )
+        ),
+        
+        floatingActionButton: FloatingActionButton(
+          //child: const Icon ( Icons.add ),
+          child: const Text('Añadir'),
+          onPressed: (){
+              print('Estoy presionando el botón');
+          },
         ),
     );
   }
